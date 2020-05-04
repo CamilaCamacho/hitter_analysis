92a5a5824eeef45f69fb086dd03872173ee97992
Camila <camila10298@gmail.com>
2020-05-03 23:49:20 -0400

    Select team and player conditional

--- strike-em-out/client/src/views/StrikeEmOut.vue
+++ strike-em-out/client/src/views/StrikeEmOut.vue
@@ -1,8 +1,37 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <h1>This is the Strike Em Out page</h1>
-    <div class="container">
+    <!--
+    <div class="container-fluid">
       <button type="button" class="btn btn-primary">{{ msg }}</button>
+    </div>
+    -->
+
+    <!-- Left Column-->
+    <div class="col">
+
+    </div>
+
+    <!-- Right Column-->
+    <div class="col">
+      <b-select v-model="selectedTeam">
+        <option disabled value='0'>Select a team</option>
+        <option v-for="team in teams" v-bind:value="team" v-bind:key="team">
+          {{ team.label }}
+        </option>
+      </b-select>
+
+      <b-select v-model="selectedPlayer">
+        <option disabled value=''>Select a player</option>
+        <option v-for="player in selectedTeam.players" v-bind:key="player">
+          {{ player }}
+        </option>
+      </b-select>
+
+      <p v-if="selectedTeam&&selectedPlayer">
+        You selected a {{ selectedTeam.label }} and specifically {{ selectedPlayer }}.
+      </p>
+
     </div>
   </div>
 </template>
@@ -16,6 +45,28 @@ export default {
   data() {
     return {
       msg: '',
+
+      selectedTeam: '0',
+      selectedPlayer: '',
+      selectedTeamLabel: '',
+      players: [],
+      teams: [
+        {
+          label: 'New York Yankees',
+          players: ['Zack Britton', 'Luis Cessa', 'Aroldis Chapman', 'Gerrit Cole'],
+          value: '1',
+        },
+        {
+          label: 'Boston Red Sox',
+          players: ['Matt Barnes', 'Ryan Brasier', 'Austin Brice', 'Nathan Eovaldi'],
+          value: '2',
+        },
+        {
+          label: 'Los Angeles Dodgers',
+          players: ['Scott Alexander', 'Pedro Báez', 'Walker Buehler', 'Caleb Ferguson'],
+          value: '3',
+        },
+      ],
     };
   },
   methods: {
@@ -30,9 +81,15 @@ export default {
           console.error(error);
         });
     },
+    selectTeam() {
+      this.selectedPlayer = '';
+      this.players = this.teams[this.selectedTeam].players;
+      this.selectedTeamLabel = this.teams[this.selectedPlayer].label;
+    },
   },
   created() {
     this.getMessage();
+    this.selectTeam();
   },
 };
 </script>
