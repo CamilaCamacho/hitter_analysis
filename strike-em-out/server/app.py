2a228a594f28030e9886d5e7b4afc18d36447b28
Momo Steele <msteel21@jhu.edu>
2020-05-08 20:30:19 -0500

    Got data from db to show on webapp

--- strike-em-out/server/app.py
+++ strike-em-out/server/app.py
@@ -1,46 +1,33 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_cors import CORS
 # from flask_mongoengine import MongoEngine
 from flask import Flask, request, Response
+from flask_pymongo import PyMongo
 from db import initialize_db
 #from models import batters
 
-COURSES = [
-    {
-        'title': 'Effective JavaScript: 68 Specific Ways to Harness the Power of JavaScript ',
-        'author': 'David Herman',
-        'paperback': True
-    },
-    {
-        'title': 'JavaScript: The Good Parts',
-        'author': 'Douglas Crockford',
-        'paperback': False
-    },
-    {
-        'title': 'Eloquent JavaScript: A Modern Introduction to Programming',
-        'author': 'Marijn Haverbeke',
-        'paperback': True
-    }
-]
-
+from pymongo import MongoClient
+client = MongoClient("mongodb://localhost:27017") # connects to client locally
+dbUsing = client["sixBatters"] # get the database
+collectionUse = dbUsing["batters"] # get the connection
+#print(collectionUse.find_one({'game_pk': 566551}))
 # configuration
 DEBUG = True
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    # name of db
-    'db': 'sixBatters',
-    # momo's host address
-    'host': '192.168.1.89',
-    # default port used
-    'port': 27017
-}
-#initialize the database
-initialize_db(app)
-
+# app.config['MONGODB_SETTINGS'] = {
+#     # name of db
+#     'db': 'sixBatters',
+#     # momo's host address
+#     'host': '192.168.1.89',
+#     # default port used
+#     'port': 27017
+# }
+# #initialize the database
+# initialize_db(app)
 
 # enable CORS
 # needed to make cross-origin AJAX requests possible
@@ -48,13 +35,15 @@ initialize_db(app)
 # r'/*' to r'/api/*'
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-
 # sanity check route
+print("success1")
 @app.route('/ping', methods=['GET'])
 # @cross_origin() # this allows CORS on a given route
 def ping_pong():
-    return jsonify('pong')
-
+    event = collectionUse.find_one({'game_pk': 566551})
+    name = event['pitcher_name']
+    return jsonify(name)
+    #return jsonify(name = name)
 
 @app.route('/courses', methods=['GET'])
 def all_courses():
@@ -64,18 +53,24 @@ def all_courses():
     })
 
 #momo added this for testing sixbatters db
-@app.route('/strikeemout')
-def get_batters():
-    Batters = batters.objects()
-    return Response(Batters, mimetype="application/json", status=200)
+# @app.route('/ping')
+# def get_batterName():
+#     name = mongo.db.sixBatters.find({"pitcher_name": "Sam Selman"})
+#     return render_template("index.html", name = name)
+#
+# #momo added this for testing sixbatters db
+# @app.route('/ping', methods=['GET'])
+# def get_batterNames():
+#     #name = sixBatters.batters({pitcher_name: "Sam Selman"})
+#     return jsonify("Momo")
 
 # #momo added this for testing sixbatters db
-@app.route('/strikeemout', methods=['GET'])
-def push_pitchtype():
-    #body = request.get_json()
-    # Batters = batters(**body).save()
-    pitch_type = Batters.pitch_type
-    return {'pitch_type': str(pitch_type)}, 200
+# @app.route('/ping', methods=['GET'])
+# def push_pitchtype():
+#     #body = request.get_json()
+#     # Batters = batters(**body).save()
+#     pitch_type = Batters.pitch_type
+#     return {'pitch_type': str(pitch_type)}, 200
 
 
 @app.route('/strikeemout', methods=['GET'])
